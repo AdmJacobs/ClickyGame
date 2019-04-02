97dd1cc4dbfc1a147cf9382d2f920e95a575ba5a
Adam <adam1acobs@gmail.com>
2019-04-02 17:45:26 -0600

    Updates

---
+++ precache-manifest.8967ca796b1731b116f58c65f523f8b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "660a1a38158bdd2d096c",
+    "url": "/ClickyGame/static/js/main.0ca195a7.chunk.js"
+  },
+  {
+    "revision": "07715ec51963695efd90",
+    "url": "/ClickyGame/static/js/2.d35d0a1e.chunk.js"
+  },
+  {
+    "revision": "660a1a38158bdd2d096c",
+    "url": "/ClickyGame/static/css/main.74d612ec.chunk.css"
+  },
+  {
+    "revision": "32c63e2ea6214951dd1096733c7a8e8b",
+    "url": "/ClickyGame/index.html"
+  }
+];
